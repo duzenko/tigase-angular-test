2fb6ba2f3a2f27a92c6bdc68117a032dcf037fd8
duzenko <anton.duzenko@gmail.com>
2021-02-04 23:02:33 +0200

    try halcyon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Inject } from '@angular/core';
 import { Component } from '@angular/core';
 import { ChatService, ChatServiceToken } from 'projects/ngx-chat/src/public-api';
 import { TestLibService } from 'projects/test-lib/src/public-api';
+import halcyon from 'halcyon-core-jsLegacy';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,13 @@ export class AppComponent {
 	title = 'test-app';
 	
 	constructor( @Inject( ChatServiceToken ) chatService: ChatService ) {
-		chatService.logIn( {
-			domain: '22.pss.com',
-			service: 'ws://localhost:5290',
-			password: '3fcb7c93-5794-4fe4-b7c2-e3d923e433da',
-			username: 'paul.garner_pavementsoft_com@22.pss.com',
-		} );
+		console.log( halcyon.tigase.halcyon.core.Halcyon())
+		// const halcyon = Halcyon();
+		// chatService.logIn( {
+		// 	domain: '22.pss.com',
+		// 	service: 'ws://localhost:5290',
+		// 	password: '3fcb7c93-5794-4fe4-b7c2-e3d923e433da',
+		// 	username: 'paul.garner_pavementsoft_com@22.pss.com',
+		// } );
 	}
 }
